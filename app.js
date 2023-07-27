a3fb6517dd306fea03bd41004039c8c908d4751b
Nicholas Leask <leask.nicholas@gmail.com>
2023-07-27 18:38:24 -0400

    feat: including routes

--- app.js
+++ app.js
@@ -32,6 +32,34 @@ app.get('/demo', (req, res) => {
   res.end()
 })
 
+app.get("/latlon", (req, res) => {
+  const street = req.query.street;
+  const city = req.query.city;
+  const state = req.query.state;
+  fetch(`https://geocoding.geo.census.gov/geocoder/geographies/address?street=${street}&city=${city}&state=${state}&benchmark=Public_AR_Census2020&vintage=Census2020_Census2020&layers=10&format=json`)
+    .then(response => response.json())
+    .then(data => res.send(data.result))
+    .catch(err => console.log('ERROR', err))
+})
+
+
+app.get("/weather", (req, res) => {
+  const lat = req.query.lat;
+  const lng = req.query.lng;
+  fetch(`https://api.weather.gov/points/${lat},${lng}`)
+    .then(response => response.json()
+      .then(response => {
+        const forecast = response.properties.forecast;
+        fetch(forecast)
+          .then(response => response.json())
+          .then(data => {
+            return res.send(data);
+          })
+          .catch(err => console.log('ERROR', err))
+      })
+    )
+})
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(
