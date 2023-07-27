af579f16db86744204321ad59654a6a87a57e0a1
Nicholas Leask <leask.nicholas@gmail.com>
2023-07-27 18:29:20 -0400

    fix: moved around app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const http = require('http');
+const port = process.env.PORT || 8080
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+
 const express = require('express');
 const cors = require('cors');
 const serverless = require('serverless-http');
@@ -43,9 +56,8 @@ router.get("/weather", (req, res) => {
     )
 })
 
-const port = 8080;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at ${port}`);
 });
 
 app.use('/.netlify/functions/api', router);
